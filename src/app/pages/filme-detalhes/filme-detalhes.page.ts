436f2055c7629a39ab362c2b25769138b30ee601
Davi Augusto Gadêlha Silva <davi-augusto.silva@serpro.gov.br>
2019-04-12 13:09:54 -0300

    Criação da página de detalhe e mudança do nome da aplicação

--- src/app/pages/filme-detalhes/filme-detalhes.page.ts
+++ src/app/pages/filme-detalhes/filme-detalhes.page.ts
@@ -1,4 +1,6 @@
+import { FilmeService } from './../../services/filme.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-filme-detalhes',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilmeDetalhesPage implements OnInit {
 
-  constructor() { }
+  informacao = null;
+
+  constructor(private activatedRoute: ActivatedRoute, private filmeService: FilmeService) { }
 
   ngOnInit() {
+    // Get the ID that was passed with the URL
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+
+    // Get the information from the API
+    this.filmeService.getDetalhes(id).subscribe(resultado => {
+      this.informacao = resultado;
+    });
+  }
+
+  abrirWebsite() {
+    window.open(this.informacao.Website, '_blank');
   }
 
 }
